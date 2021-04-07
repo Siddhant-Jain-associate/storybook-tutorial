728ecda0a1abc8c7a7cc7a4fc343e9210f7e1c57
Siddhant-Jain-associate <siddhant.jain@successive.tech>
2021-04-07 10:40:43 +0530

    Added register form

---
+++ src/components/Form/RegisterForm/RegisterForm.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Button from '../../Button/Button';
+import Input from '../../Input/Input';
+
+const RegisterForm=({formname})=> {
+    return (
+        <form>
+            <fieldset>
+                <legend><h2>{formname}</h2></legend><br/>
+            <Input type="text" name="Name" placeholder="Enter your FullName" /><br/>
+            <Input type="email" name="Email" placeholder="Enter your email" /><br/>
+            <Input type="password" name="Password" placeholder="Enter your password" /><br/>
+            <Input type="password" name="Confirm password" placeholder="Confirm Password" /><br/>
+            <Button label="Register" />
+            </fieldset>
+        </form>
+    )
+}
+
+export default RegisterForm
---
+++ src/components/Form/RegisterForm/Registerform.stories.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Register from './RegisterForm';
+
+export default{
+    title:'Registeration',
+    component:Register
+}
+
+
+const Template = (args) => <Register {...args}/>
+
+export const SignUp = Template.bind({});
+
+SignUp.args={
+    formname:"RegisterForm"
+}
